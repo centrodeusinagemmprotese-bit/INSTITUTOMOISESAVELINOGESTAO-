62d7d07fb20ac4674e3bbab5a43a53aea21aabaf
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 16:09:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,17 @@ export default defineConfig({
   plugins: [react()],
   base: './', // base relativa para deploy
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, 'src')
+    }
+  },
+  base: './',
+})
