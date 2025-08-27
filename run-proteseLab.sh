6800f0a4c63fc2bab92c00926afdaceacd37fa84
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 17:44:12 -0300

    Delete run-proteseLab.sh

--- run-proteseLab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Instalando dependências..."
-npm install || { echo "Erro ao instalar dependências"; exit 1; }
-
-echo "Iniciando servidor de desenvolvimento..."
-npm run dev || { echo "Erro ao iniciar o servidor"; exit 1; }
-
-echo "Servidor iniciado. Pressione Ctrl+C para parar."
-read -p "Pressione Enter para sair..."
