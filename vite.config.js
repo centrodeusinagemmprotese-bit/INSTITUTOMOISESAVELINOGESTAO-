01b31771425882ed61e443ef1a57c2f5f18c92d7
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 16:51:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // base relativa para deploy no Vercel
+  base: './', // Caminhos relativos para assets
 })
