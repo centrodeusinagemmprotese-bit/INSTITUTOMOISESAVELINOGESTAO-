252b67a258c7c2f392e20711f4486934a13b3f13
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 15:04:55 -0300

    Create Navbar.jsx

---
+++ src/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav style={{ background: '#0077b6', padding: '1rem', color: '#fff' }}>
+      ProteseLab
+    </nav>
+  );
+}
