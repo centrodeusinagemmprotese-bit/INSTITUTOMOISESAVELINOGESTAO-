dcff01dd9944d55cc665c6bd9fd87aadd8d00e19
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 16:18:00 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
-import Navbar from './components/Navbar'; // relativo
-import Clientes from './pages/Clientes';  // relativo
+import React from 'react';
+import Navbar from './components/Navbar';
+import Clientes from './pages/Clientes';
 import Servicos from './pages/Servicos';
 import Financeiro from './pages/Financeiro';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Clientes />
+        <Servicos />
+        <Financeiro />
+      </div>
+    </>
+  );
+}
+
+export default App;
