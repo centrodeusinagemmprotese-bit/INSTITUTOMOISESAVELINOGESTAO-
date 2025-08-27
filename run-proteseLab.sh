0a54297d0f22a546b6aecdf1e44cc993f869dd89
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 16:12:21 -0300

    Update run-proteseLab.sh

--- run-proteseLab.sh
+++ run-proteseLab.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# proteseLab-react-vercel - Script de instalação e execução
-echo "Instalando dependências..."
 npm install
-echo "Rodando aplicação em modo desenvolvimento..."
 npm run dev
