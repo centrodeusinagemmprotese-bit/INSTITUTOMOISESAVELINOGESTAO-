e7fcfda875ddef5cda43566348de150efae9ec1b
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 16:14:53 -0300

    Update run-proteseLab.bat

--- run-proteseLab.bat
+++ run-proteseLab.bat
@@ -1,7 +1,2 @@
-@echo off
-REM proteseLab-react-vercel - Script de instalação e execução
-echo Instalando dependências...
 npm install
-echo Rodando aplicação em modo desenvolvimento...
 npm run dev
-pause
