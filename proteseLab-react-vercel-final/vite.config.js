751f9eef0be9c36b02290d1cb10f3f7826a3be69
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 18:21:51 -0300

    Update vite.config.js

--- proteseLab-react-vercel-final/vite.config.js
+++ proteseLab-react-vercel-final/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // caminhos relativos
+  resolve: { alias: { 'src': path.resolve(__dirname, 'src') } }
 })
-
