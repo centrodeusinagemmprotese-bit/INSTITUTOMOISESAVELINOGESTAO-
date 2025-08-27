59cd54fcf5721495116fdb55825516996843f8eb
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 14:55:52 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,13 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; export default defineConfig({ plugins: [react()] });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    rollupOptions: {
+      // Caso precise externalizar algo
+      external: []
+    }
+  }
+})
