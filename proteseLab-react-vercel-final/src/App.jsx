099061fd3c3a7d7cf54ee4f43c14076477243d3c
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 17:14:09 -0300

    Update App.jsx

--- proteseLab-react-vercel-final/src/App.jsx
+++ proteseLab-react-vercel-final/src/App.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Clientes from './pages/Clientes';
+import Servicos from './pages/Servicos';
+import Financeiro from './pages/Financeiro';
 
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Clientes />
+        <Servicos />
+        <Financeiro />
+      </div>
+    </>
+  );
+}
+
+export default App;
