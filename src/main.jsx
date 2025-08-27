27d1f22c5dcc4b9bfc3f3828225b4ad57eb6c1c6
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 16:18:22 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // correto
-import './index.css';    // correto
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
