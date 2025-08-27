7c27c17caa9ff2220095ef28b08a9f2c80c4428b
centrodeusinagemmprotese-bit <centrodeusinagemmprotese@gmail.com>
2025-08-27 18:25:54 -0300

    Add files via upload

---
+++ proteseLab-react-vercel-final/backend/routes/servicos.js
@@ -0,0 +1,1 @@
+import express from "express"; const router = express.Router(); router.get("/", (req,res)=>res.send("Rota servicos")); export default router;
---
+++ proteseLab-react-vercel-final/backend/routes/usuarios.js
@@ -0,0 +1,1 @@
+import express from "express"; const router = express.Router(); router.get("/", (req,res)=>res.send("Rota usuários")); export default router;
